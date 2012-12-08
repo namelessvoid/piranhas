c5b9bc20c08d3fcc61c07fe5afeb08631559ddc6
namelessvoid <pommesdiefritte@gmx.de>
2012-12-08 17:30:34 +0100

    Implemented configparser which loads the serverconfigurations in server.cfg on start up.

--- serverlauncher.py
+++ serverlauncher.py
@@ -1,15 +1,36 @@
+from ConfigParser import RawConfigParser
+from datetime import datetime
+import random
+
 from nibbles.server.network.server import Server
 from nibbles.server.commandprocessor import CommandProcessor
 from nibbles.server.engine import Engine
 
-import random
+# read in the configuration file.
+configparser = RawConfigParser()
+configparser.read("server.cfg")
 
+port = configparser.getint("network", "port")
+host = configparser.get("network", "host")
+starttime = configparser.get("engine", "starttime")
+starttime = datetime.strptime(starttime, "%y.%m.%d/%H:%M:%S")
+foodpernibble = configparser.getint("engine", "foodpernibble")
+fieldspernibble = configparser.getint("engine", "fieldspernibble")
+
+# instantiate objects
 commandprocessor = CommandProcessor()
-server = Server(commandprocessor, 'localhost', 1234, 1)
+server = Server(commandprocessor, host, port, 1)
 commandprocessor.setserver(server)
 random = random.Random()
 engine = Engine(random)
+
+# set up configurations
 commandprocessor.setengine(engine)
 engine.setcmp(commandprocessor)
 
+engine.setfoodpernibble(foodpernibble)
+engine.setfieldspernibble(fieldspernibble)
+engine.setgamestart(starttime)
+
+# run the server
 server.setjoin()
