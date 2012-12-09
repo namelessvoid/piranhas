bae3b4f67141f9406ec60b63166e86dc7f84447d
Patrick Link <patricklinkey@web.de>
2012-12-09 16:35:12 +0100

    Updated some methods and regular expressions. DummyServer / DummyClient is only for testing

--- nibbles/client/engine.py
+++ nibbles/client/engine.py
@@ -3,6 +3,45 @@
 import re
 import sys
 import threading
+from socket import *
+from nibbles.nibblelogger import *
+from nibbles.board import *
+from nibbles.client.network.networkinterface import *
+
+class DummyServer(threading.Thread): #to be removed
+    def __init__(self, host, port):
+        threading.Thread.__init__(self)
+        self._b = Board(10,10)
+        self._s = socket.socket()
+        self._s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self._s.bind((host,port))
+        self._s.listen(1)
+        self.start()
+
+    def run(self):
+        c, (clienthost, clientport) = self._s.accept()
+        c.sendall("5;....<...a**.**<<<j*......;@")
+        data = c.recv(1024)
+        print("Vom Server: "+data+"\n")
+
+class DummyClient(): #to be removed
+    def __init__(self, host, port):
+        self._host = host
+        self._port = port
+        self._s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.verbinden()
+
+    def verbinden(self):
+        self._s.connect((self._host,self._port))
+
+    def receivemessage(self):
+        return self._s.recv(1024)
+
+    def sendmessage(self, message):
+        self._s.sendall(message)
+
+
+
 
 class Engine(threading.Thread):
 
@@ -12,47 +51,39 @@ class Engine(threading.Thread):
         self._ni = ni
         self._board = board
         self._ai = ai
+        self._logger = NibbleStreamLogger("client.engine")
 
     def recievecommand(self):
         """Recieves a message from networkinterface and return it"""
-        command = self._ni.recieveMessage()
+        command = self._ni.receivemessage()
         return command
+
+    def sendcommand(self, command):
+        self._ni.sendmessage(command)
 
     def registrationpossible(self):
         """Checks whether registration is possible on server"""
-        self._ni.sendMessage("anmeldung moeglich@")
+        self._ni.sendmessage("anmeldung moeglich@")
 
     def connecttoserver(self):
         """Connects to the Server"""
-        self._ni.sendMessage("anmelden@")
+        self._ni.sendmessage("anmelden@")
 
     def getworldsize(self):
         """Gets the size of the world"""
-        self._ni.sendMessage("weltgroesse@")
+        self._ni.sendmessage("weltgroesse@")
 
     def startgame(self):
         """Starts the game"""
-        self._ni.sendMessage("spielbeginn@")
-
-    def movetoken(self, x, y, newX, newY):
-        """Moves a token on the board"""
-        self._board.moveToken(x, y, newX, newY)
-
-    def settoken(self, token, x, y):
-        """Sets a token on the board"""
-        self._board.setToken(token, x, y)
-
-    def gettoken(self, x, y):
-        """Gets the token on the spezified position of the board"""
-        self._board.getToken(x,y)
+        self._ni.sendmessage("spielbeginn@")
 
     def printmessage(self, message):
         """prints a message to stdout"""
         sys.stdout.write(message)
 
-    def paintboard(self):
+    def printboard(self):
         """Renders the board"""
-        self._board.paintBoard()
+        BoardRenderer.printboard(self._board)
 
     def run(self):
         """Commandprocessor for the engine:
@@ -65,11 +96,13 @@ class Engine(threading.Thread):
             "nein@" : (self.printmessage,"Anmeldung nicht moeglich"),
             "\D{1}@" : self.startgame,
             "\d+x\d+@" : (self.printmessage,""),
-            "\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}@" : (self.printmessage,"")
+            "\d{4}\-\d{2}\-\d{2} \d{2}:\d{2}:\d{2}@" : (self.printmessage,""), #do something
+            "(;|\d{1};)(;|[*><=.\D]{25};|ende;)(@|[*><=.\D]*@)" : (self.printmessage,"") #self.givetoai(message)
         }
         #while 1:
-        #command = self.recieveCommand()
-        command = "2012-11-17 00:00:00@"
+        self._logger.info(" Waiting for command...")
+        command = self.recievecommand()
+        self._logger.info(" Recieved command: %s" % command)
         for i, key in enumerate(functions):
             if command == key:
                 if isinstance(functions[command], tuple):
@@ -94,15 +127,15 @@ class Engine(threading.Thread):
                     functions[key]()
                     break
             elif i == len(functions)-1:
-                self.printmessage("fehler@")
+                self.printmessage("fehler@\n")
                 break
 
 if __name__== "__main__":
-    sys.path.insert(0,"../")
 
-    #brd = board.board(5,5)
-    #ai = AI()
+    server = DummyServer('', 1234)
+    client = DummyClient("localhost", 1234)
 
-    engine = Engine()
-    #engine.paintboard()
+    #ni = NetworkInterface("localhost", 1234)
+
+    engine = Engine(client)
     engine.run()
