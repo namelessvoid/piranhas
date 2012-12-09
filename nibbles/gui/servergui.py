493d159b5abc8558197dc706ed452755ca69757a
ruction <benjaminwoehrl@googlemail.com>
2012-12-09 18:59:41 +0100

    updated servergui.py

--- nibbles/gui/servergui.py
+++ nibbles/gui/servergui.py
@@ -4,27 +4,15 @@ from logging import log
 
 class ServerGui(QtGui.QMainWindow):
     def __init__(self):
-        app = QtGui.QApplication(sys.argv)
-        self.ui = uic.loadUi("servergui.ui")
-        self.ui.show()
+        QtGui.QMainWindow.__init__(self)
+        self.ui = uic.loadUi("servergui.ui", self)
 
-        i = 0
-        while i < 100:
+        for i in range(100):
             text = "Logger 08:08:2012 - INFO: Test logger" + str(i)
             self.ui.logger.append(text.rstrip())
-            i += 1
-
-        sys.exit(app.exec_())
-
-
-#    def log(self, text):
-#        # Write into the log window and remove "\n" if necessary
-#        self.ui.logger.append(text)
-#
-#        # Scroll to the end
-#        cursor = self.ui.logTextEdit.textCursor()
-#        cursor.movePosition(QTextCursor.End)
-#        self.ui.logTextEdit.setTextCursor(cursor)
 
 if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
     servergui = ServerGui()
+    servergui.ui.show()
+    sys.exit(app.exec_())
