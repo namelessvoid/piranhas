ae61d13c7c0a00351be3d0a57aa27d9226028fb2
Patrick Link <patricklinkey@web.de>
2012-12-12 21:50:06 +0100

    Fixed some connection problems

--- nibbles/client/network/networkinterface.py
+++ nibbles/client/network/networkinterface.py
@@ -7,7 +7,8 @@
 
 import socket
 import logging
-
+import thread
+from nibbles.nibblelogger import *
 
 class NetworkInterface(socket.socket):
     #_s, _host, _port = (None, None, None)
@@ -17,41 +18,48 @@ class NetworkInterface(socket.socket):
         host:    string that holds the address of the host
         port:    integer that holds the destination port"""
         socket.socket.__init__(self, socket.AF_INET, socket.SOCK_STREAM)
+        self._buffer = False
         self._host = host
         self._port = port
+        self._logger = NibbleStreamLogger("client.networkinterface")
         self.connect((self._host, self._port))
-        logging.info("NetworkInterface connected to %s:%d" % (host, port))
+        self._logger.info("NetworkInterface connected to %s:%d" % (host, port))
+        try:
+            self.receivemessageThread = thread.start_new_thread(self.receivemessage, ())
+        except thread.error:
+            self._logger.info("Error at connection, cant start thread")
 
     def sendmessage(self, message):
         """Sends a message to the host.
         message: string representation of the message."""
-        logging.info("NetworkInterface.sendMessage(\"" + message + "\")")
+        self._logger.info("NetworkInterface.sendMessage(\"" + message + "\")")
         socketfile = self.makefile()
         socketfile.write(message)
-        logging.info("NetworkInterface.sendMessage() DONE")
+        #logging.info("NetworkInterface.sendMessage() DONE")
         socketfile.close()
+
+    def getmessage(self):
+        if self._buffer:
+            tmp = self._buffer
+            self._buffer = False
+            return tmp
+        else:
+            return False
 
     def receivemessage(self):
         """Receives a message from the host.
         return value: String which holds the message"""
-        logging.info("NetworkInterface().receiveMessage()")
-        message = ""
-        self.setblocking(0)
+        self._logger.info("NetworkInterface().receiveMessage()")
         try:
             while 1:
                 data = self.recv(1024)
                 if not data:
                     break
-                logging.info("received: " + str(data))
-                message += data
+                self._logger.info("received: " + str(data))
+                self._buffer = data
         except socket.error, (errno, string):
-            if errno == 11:
-                return message
-            else:
-                print str(errno) + string
-        finally:
-            self.setblocking(1)
-        return message
+            self._logger.info("Error at receiving a message")
+
 
 if __name__ == "__main__":
     ni = NetworkInterface("localhost", 50007)
