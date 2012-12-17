6880d9a7393aa80de88016dd1e5954a733721845
Patrick Link <patricklinkey@web.de>
2012-12-17 19:31:14 +0100

    Added dialogs for host and port

--- nibbles/gui/clientgui.py
+++ nibbles/gui/clientgui.py
@@ -9,7 +9,6 @@ class ClientGui(QtGui.QMainWindow):
         self._ui = uic.loadUi("./nibbles/gui/clientgui.ui", self)
         self._engine = engine
         self._logger = NibbleQTextEditLogger(self._ui.logger, "client.gui")
-        self._viewwidth = 5
 
         #register the update method for gui
         self._engine.registermethod(self.updategui)
@@ -24,40 +23,26 @@ class ClientGui(QtGui.QMainWindow):
         self._ui.stopgame.clicked.connect(self.gamestop)
 
     def updategui(self):
-#        view = self._engine.getcurrentview()
-#        if view:
-#            boardstring = ''
-#            for i in range(( len(view) / self._viewwidth )):
-#                i *= self._viewwidth
-#                boardstring += (view[i : self._viewwidth + i] + '\r\n')
-#
-#            self._ui.field.setText(boardstring)
-#            self._ui.field.update()
-
-        self.c = 0
-        if self.c == 0:
-            self._ui.boardrenderer.setboard(boardfromstring(self._engine.getcurrentview(), 5, 5))
-            self.c += 1
-
+        self._ui.boardrenderer.setboard(boardfromstring(self._engine.getcurrentview(), 5, 5))
         self._ui.boardrenderer.update()
         self.update()
 
     def gamestart(self):
         try:
-#            self._engine.connecttoserver(unicode(self._ui.ipInput.toPlainText()),
-#                int(unicode(self._ui.portInput.toPlainText())))
-            self._engine.connecttoserver("localhost", 1234)
+            host, okhost = QtGui.QInputDialog.getText(self, "Host Settings", "Host:")
+            if not okhost: return
+            port, okport = QtGui.QInputDialog.getInt(self, "Port Settings", "Port:")
+            if not okport: return
+            self._logger.info("Host: %s" % host)
+            self._logger.info("Port: %d" % port)
+            self._engine.connecttoserver(host, port)
             self._engine.start()
             self._engine.sendcommand("anmeldung moeglich@")
         except:
             self._logger.warning("Cant connect to the server!")
 
-
     def gamestop(self):
         self._engine.stoploop()
-
-    def renderBoard(self):
-        pass
 
 
 
