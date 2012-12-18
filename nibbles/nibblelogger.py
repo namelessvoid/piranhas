8199070fbea66ca73c9f3181b6c580a1e534eb17
namelessvoid <pommesdiefritte@gmx.de>
2012-12-18 14:35:17 +0100

    Removed NibbleQTextEditLogger and introduced use of logsignal.

--- nibbles/nibblelogger.py
+++ nibbles/nibblelogger.py
@@ -1,6 +1,8 @@
 import logging
 import threading
 
+from nibbles.nibblesignal import NibbleStringSignal
+
 class NibbleLogger(object):
     def __init__(self, logger="default.default", level=logging.DEBUG):
         self._logger = logging.getLogger(logger)
@@ -8,24 +10,31 @@ class NibbleLogger(object):
         self._formatter = logging.Formatter("%(asctime)s %(levelname)s: %(name)s: %(message)s", "%d.%m.%Y %H:%M:%S")
         self.setFormatter(self._formatter)
         self._logger.addHandler(self)
+        self.logsignal = NibbleStringSignal()
 
     def log(self, level, message):
         self._logger.log(level, message)
+        self.logsignal.callstr(message)
 
     def debug(self, message):
         self._logger.debug(message)
+        self.logsignal.callstr(message)
 
     def info(self, message):
         self._logger.info(message)
+        self.logsignal.callstr(message)
 
     def error(self, message):
         self._logger.error(message)
+        self.logsignal.callstr(message)
 
     def warning(self, message):
         self._logger.warning(message)
+        self.logsignal.callstr(message)
 
     def critical(self, message):
         self._logger.critical(message)
+        self.logsignal.callstr(message)
 
 class NibbleStreamLogger(logging.StreamHandler, NibbleLogger):
     def __init__(self, logger="default.default", level=logging.DEBUG, stream=None):
@@ -37,17 +46,17 @@ class NibbleFileLogger(logging.FileHandler, NibbleLogger):
         logging.FileHandler.__init__(self, filename, filemode)
         NibbleLogger.__init__(self, logger, level)
 
-class NibbleQTextEditLogger(logging.Handler, NibbleLogger):
-    def __init__(self, text_edit, logger="default.default", level=logging.DEBUG):
-        logging.Handler.__init__(self)
-        NibbleLogger.__init__(self, logger, level)
-        self.text_edit = text_edit
-        self.logmessages = []
+#class NibbleQTextEditLogger(logging.Handler, NibbleLogger):
+    #def __init__(self, text_edit, logger="default.default", level=logging.DEBUG):
+        #logging.Handler.__init__(self)
+        #NibbleLogger.__init__(self, logger, level)
+        #self.text_edit = text_edit
+        #self.logmessages = []
 
-    def emit(self, record):
-        lock = threading.Lock()
-        with lock:
-            self.text_edit.append(self.format(record))
+    #def emit(self, record):
+        #lock = threading.Lock()
+        #with lock:
+            #self.text_edit.append(self.format(record))
 
 if __name__ == "__main__":
     my_logger = NibbleStreamLogger()
