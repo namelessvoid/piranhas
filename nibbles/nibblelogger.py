8d3ecd9640b0cd189f9875e1a24d671586a4bc2f
namelessvoid <pommesdiefritte@gmx.de>
2012-12-20 20:15:53 +0100

    Added QtHandler to logging framework which now formats the logging messages displayed in the gui.

--- nibbles/nibblelogger.py
+++ nibbles/nibblelogger.py
@@ -11,62 +11,59 @@ class NibbleLogger(object):
         self.setFormatter(self._formatter)
         self._logger.addHandler(self)
         self.logsignal = NibbleStringSignal()
+        qthandler = QtHandler(self.logsignal, level)
+        qthandler.setFormatter(self._formatter)
+        self._logger.addHandler(qthandler)
 
     def log(self, level, message):
         self._logger.log(level, message)
-        self.logsignal.callstr(message)
 
     def debug(self, message):
         self._logger.debug(message)
-        self.logsignal.callstr(message)
 
     def info(self, message):
         self._logger.info(message)
-        self.logsignal.callstr(message)
 
     def error(self, message):
         self._logger.error(message)
-        self.logsignal.callstr(message)
 
     def warning(self, message):
         self._logger.warning(message)
-        self.logsignal.callstr(message)
 
     def critical(self, message):
         self._logger.critical(message)
-        self.logsignal.callstr(message)
+
 
 class NibbleStreamLogger(logging.StreamHandler, NibbleLogger):
     def __init__(self, logger="default.default", level=logging.DEBUG, stream=None):
         logging.StreamHandler.__init__(self, stream)
         NibbleLogger.__init__(self, logger, level)
 
+
 class NibbleFileLogger(logging.FileHandler, NibbleLogger):
     def __init__(self, logger="default.default", level=logging.DEBUG, filename="testlog.txt", filemode="a"):
         logging.FileHandler.__init__(self, filename, filemode)
         NibbleLogger.__init__(self, logger, level)
 
-#class NibbleQTextEditLogger(logging.Handler, NibbleLogger):
-    #def __init__(self, text_edit, logger="default.default", level=logging.DEBUG):
-        #logging.Handler.__init__(self)
-        #NibbleLogger.__init__(self, logger, level)
-        #self.text_edit = text_edit
-        #self.logmessages = []
 
-    #def emit(self, record):
-        #lock = threading.Lock()
-        #with lock:
-            #self.text_edit.append(self.format(record))
+class QtHandler(logging.Handler):
+    """A custom implementation of handler that handles the
+        logging for Qt guis."""
+    def __init__(self, logsignal, level=logging.DEBUG):
+        """Constructor of QtHandler.
+            Arguments:
+                logsignal -- (nibbles.nibblesignal.NibbleStringSignal) the
+                             signal that is emitted when something is logged.
+                level    --  (logging.LEVEL) a loglevel specified in the
+                             python logging framework."""
+        super(QtHandler, self).__init__(level)
+        self._logsignal = logsignal
 
-if __name__ == "__main__":
-    my_logger = NibbleStreamLogger()
-    my_logger.info("Testnachricht")
-    my_logger.warning("Testnachricht")
-    my_logger.critical("Testnachricht")
-    my_logger.error("Testnachricht")
-    my_logger.log(logging.ERROR, "Testnachricht")
-
-    #my_logger2 = NibbleFileLogger()
-    #my_logger2.info("Testnachricht von FileLogger")
-
-
+    def handle(self, record):
+        """Overwrite the logging.Handler.handle() method. This method
+            just emits the logsignal that is processes within the gui.
+            Arguments:
+                record -- (logging.record) record file of pythons logging
+                          framework"""
+        msg = self.format(record)
+        self._logsignal.callstr(msg)
